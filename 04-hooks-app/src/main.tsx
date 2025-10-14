21e2c08c820d585a9845f1d595f069999dcbfd1e
Raul <carbonoraul2@gmail.com>
2025-10-14 16:51:05 -0500

    useReducer

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { PokemonPage } from './03-examples/PokemonPage'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
+import { ScrambleWords } from './05-useReducer/ScrambleWords'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 )
